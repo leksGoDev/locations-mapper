e696ce4e6174562ba70cf87d201651a313f79d17
leksGo <Gan-96@yandex.ru>
2021-12-10 23:48:05 +0300

    First CSS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-function App() {
-  return (
-    <div>
+import './App.css'
+export default function App() {
+    return (
+        <div className="wrapper">
+            <div className="container">
+                <header>My router</header>
+                <div className="content">
+                    <div>Routes</div>
+                    <div>Map</div>
+                </div>
+                <footer>by leksGo</footer>
+            </div>
     </div>
   );
 }
-
-export default App;
